755f5c57cdaded59196a92ef0e3e3e5a8bf27fc7
Arslan Wali <arsalwali7@gmail.com>
2024-09-13 16:41:50 +0500

    Basic JS

---
+++ Chapter 1/01_Basic.js
@@ -0,0 +1,21 @@
+const accountId = 1234
+let accountEmail= "Arslan@google.com"
+var accountPassword= "112233"
+accountCity= "Jalalabad"
+
+// accountId=2 This is not allowed to do we cant change const value if this is already decleared \
+
+console.log(accountId)
+
+accountEmail="Wali@google.com"
+accountPassowrd="Abc"
+accountCity="Jalalabad"
+let accountState;
+
+/*
+prefer not to use var and also not use only name : Just const or let when we use const the value for that 
+variable will not change and when we use let we can change the value for that variable.
+when we use var we will face issue in block scope and funciton scope.
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
