72c97c87f0f9308979257ae3cb6e1333235fa764
Arslan Wali <arsalwali7@gmail.com>
2024-09-13 17:23:25 +0500

    dataTypes

---
+++ Chapter 1/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all code as JS newer version
+
+// alert(2+3) this will not run beacuse we are using nodejs engine not browser
+
+console.log(9+5)
+
+console.log("Arslan Wali")  // There must be code readablity dont mesh the code
+console.log("Jalalabad")
+
+let name = "Arslan Wali"
+let age = 24
+let isLoggedIn = false
+
+//number => 2 power 53 if we want to store big value we use big int 
+// String => ""
+// boolen => true or false 
+// null => standalone value
+// undefined => not assign value
+// Symbol => show uqinuenss
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
+
+
