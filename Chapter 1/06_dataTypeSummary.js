770609eb017218e5d5e4eb847b5232448e45a651
Arslan Wali <arsalwali7@gmail.com>
2024-09-15 13:14:05 +0500

    summaryDataType

---
+++ Chapter 1/06_dataTypeSummary.js
@@ -0,0 +1,38 @@
+// primitive 
+// Reference or Non primitive Data type
+
+// Primitive (Stings,Number,boolean,bigInt,symbol,null,undefined )
+
+
+
+const score = 100
+const socreValue = 110.3
+
+const isLoggedIn = false
+
+const outSideTemp = null
+
+let userEmail;
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+console.log(id == anotherId);
+
+const bigInt = 23124123324234432131n
+
+// Reference (Array, function,objects)
+
+const heros = ["Arslan","Wali","Jalalabad"]
+
+let myObj = {
+    Name : "Arslan",
+    Age : 27,
+}
+
+const myFunction = function(){
+
+    console.log("Hello Arslan ");
+    
+}
+
