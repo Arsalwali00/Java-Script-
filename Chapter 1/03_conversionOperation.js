74159f258b7dcdbdd857d19db378ac9fd52f792a
Arslan Wali <arsalwali7@gmail.com>
2024-09-15 09:51:02 +0500

    dataTypeConversion

---
+++ Chapter 1/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "34"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+console.log(typeof valueInNumber);
+
+
+
+let isLoggedIn=1
+let boolenIsLoggedIn= Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+
+// 1 => Ture  0=> false
+// "" => false
+// "Arslan" => True
+
+let isSomeNumber = 22
+let stringNumber = String(isSomeNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber)
