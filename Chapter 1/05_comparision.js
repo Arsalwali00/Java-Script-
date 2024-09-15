39dafe252a164807651fe6fefa2b3a2b8fe25094
Arslan Wali <arsalwali7@gmail.com>
2024-09-15 12:46:50 +0500

    dataTypeConversion

---
+++ Chapter 1/05_comparision.js
@@ -0,0 +1,30 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+console.log(2>=1);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);    // in most cases we dont use these type of conversion beacuse we have to take care of code readability
+
+
+console.log(undefined=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+
+console.log("2"===2); // in this type of compresion === JS take care of Datatype while comparing 
+
+
+
+
+
+
+
+
+
+
+
