d511c4b6ff10597ffe046b722b5c81850acb221b
Arslan Wali <arsalwali7@gmail.com>
2024-09-15 11:15:36 +0500

    dataTypeConversion

---
+++ Chapter 1/04_conversionOperation_2.js
@@ -0,0 +1,28 @@
+
+console.log(2+2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+
+let str1 = "Arslan"
+let str2 = " Wali"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+1+"2");
+console.log("1"+"1"+2+2);
+console.log(1+1+"2"+"2");
+
+console.log(+true); 
+console.log(+"") // these both are correct but avoide to use this
+
+
+
+let gameCounter = 10
+gameCounter++
+console.log(gameCounter); // read about post prefix and pre prefix
+
