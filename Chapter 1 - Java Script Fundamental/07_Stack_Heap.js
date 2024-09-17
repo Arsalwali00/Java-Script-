28a5306bd4659af7beb955b325b3d74b96845fdc
Arslan Wali <arsalwali7@gmail.com>
2024-09-17 08:14:49 +0500

    Stack and Heap

---
+++ Chapter 1 - Java Script Fundamental/07_Stack_Heap.js
@@ -0,0 +1,35 @@
+// Stack (Primitives ) and Heap (Non Primitives)
+
+//____________Stack ________________________________
+
+let myYoutubeChannelName="Arslan Wali"
+
+let anotherName="Computer Vision"
+
+anotherName="Wali"
+
+console.log(myYoutubeChannelName);
+
+
+console.log(anotherName);
+
+//_________________Heap________________________________
+
+let userOne = {
+
+    email:"wali@google.com",
+    password:"123456",
+}
+
+let userTwo = {
+    email:"arslan@gmail.com",
+    password:"654321",
+}
+
+userTwo=userOne
+
+userTwo.email="arsalwali@yahoo.com"
+
+console.log(userOne.email);
+
+console.log(userTwo.email);
